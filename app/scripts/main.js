40ba884caa75169537eb0c456074e663a7c6af9d
Therese <therese@therese.com>
2015-07-24 16:18:46 -0700

    Initial commit

---
+++ app/scripts/main.js
@@ -0,0 +1,23 @@
+console.log('\'Allo \'Allo!');
+
+
+$(window).scroll(function() {
+	'use strict'
+    if ($(".navbar").offset().top > 50) {
+        $(".navbar-fixed-top").addClass("top-nav-collapse");
+    } else {
+        $(".navbar-fixed-top").removeClass("top-nav-collapse");
+    }
+});
+
+//jQuery for page scrolling feature - requires jQuery Easing plugin
+$(function() {
+	'use strict'
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
